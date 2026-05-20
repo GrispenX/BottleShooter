aa577fbb77933d817d27ec52f6b43fb8a54db667
aharook <146359839+aharook@users.noreply.github.com>
2026-05-20 21:07:53 +0300

    add Settings

--- Assets/Scripts/GameStart.cs
+++ Assets/Scripts/GameStart.cs
@@ -1,16 +1,47 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameStart : MonoBehaviour
 {
+    [Header("Панель налаштувань")]
+    public GameObject settingsPanel;
+
+    private void Start()
+    {
+
+        if (settingsPanel != null)
+        {
+            settingsPanel.SetActive(false);
+        }
+    }
+
     public void LoadScene(string sceneName)
     {
         Debug.Log($"Loading scene: {sceneName}");
-        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+        SceneManager.LoadScene(sceneName);
     }
 
     public void LoadGameScene()
     {
         LoadScene("SampleScene");
+    }
+
+    public void OpenSettings()
+    {
+
+        if (settingsPanel != null)
+        {
+            settingsPanel.SetActive(true);
+
+        }
+    }
+
+    public void CloseSettings()
+    {
+        if (settingsPanel != null)
+        {
+            settingsPanel.SetActive(false);
+        }
     }
 
     public void QuitGame()
@@ -19,4 +50,3 @@ public class GameStart : MonoBehaviour
         Application.Quit();
     }
 }
-
